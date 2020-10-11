a476991d526ef1405b58f5c70b4fb13dde303280
Xiao Zhang <zhangx@gbctech.cn>
2020-10-11 19:54:23 +0800

    minor file header

--- client/rpc-api/src/distaff_vm/error.rs
+++ client/rpc-api/src/distaff_vm/error.rs
@@ -1,13 +1,18 @@
-/**
- * @file error.rs
- * @author ming (wmzhou@protonmail.com)
- * @brief Distaff RPC errors.
- * @version 1.00
- * @date 2020-10-03
- *
- * @copyright Copyright@gbctech. All Rights Reserved.
- *
- */
+// @file mod.rs
+// @author ming (wmzhou@protonmail.com)
+// @brief distaffvm rpc errors.
+// @version 0.0.1
+// @date 2020-10-03
+
+// License Apache 2.0
+// Copyright (C) 2020 Starks Network
+// This file is part of the Starks Node program.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// You should have received a copy of the Apache License 2.0
+// along with this program. If not, see <https://www.apache.org/licenses/LICENSE-2.0>.
+
  use jsonrpc_core as rpc;
 
  /// Distaff RPC Result type.
