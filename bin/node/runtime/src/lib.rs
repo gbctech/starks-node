8c8105d6558e4657624eff22fcb363e7e9c9aab1
dejavukong <foxcong.yolo@gmail.com>
2020-11-30 21:52:33 +0800

    add distaffvm in runtime

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -897,6 +897,10 @@ impl pallet_vesting::Trait for Runtime {
 	type WeightInfo = weights::pallet_vesting::WeightInfo<Runtime>;
 }
 
+impl pallet_distaff_vm::Trait for Runtime {
+	type Event = Event;
+}
+
 construct_runtime!(
 	pub enum Runtime where
 		Block = Block,
@@ -935,6 +939,7 @@ construct_runtime!(
 		Scheduler: pallet_scheduler::{Module, Call, Storage, Event<T>},
 		Proxy: pallet_proxy::{Module, Call, Storage, Event<T>},
 		Multisig: pallet_multisig::{Module, Call, Storage, Event<T>},
+		DistaffVM: pallet_distaff_vm::{Module, Call, Storage, Event<T>},
 	}
 );
 
